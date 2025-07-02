ae85bf65935eeeb61c80e125c969f36adf49ecc3
SICROMONOCO <bilal06siki@gmail.com>
2025-07-02 21:26:52 +0100

    Initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import Terminal from "@/components/Terminal";
+
+function App() {
+  return (
+    <div className="h-screen w-full bg-black overflow-hidden touch-manipulation">
+      <Terminal />
+    </div>
+  );
+}
+
+export default App;
